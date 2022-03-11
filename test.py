42d2ed0fd8923b008feab03830d1e4d0aecf851e
kkstar1988 <49004044+kkstar1988@users.noreply.github.com>
2022-03-10 21:50:56 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+from Getdata import *
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import os
+import xlwings as xw
+import akshare as ak
+import yfinance as yf
+import datetime as dt
+from datetime import timedelta
+
+import matplotlib.pyplot as plt
+
+'''
+# Stocks Summary
+## by Xin Xu
+
+The comparison
+'''
